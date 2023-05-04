d7bef619bf98b6d8f7737222400f482a4b758bf8
Sariot Hossain <106438276+sariothossain1011@users.noreply.github.com>
2023-05-05 02:34:46 +0600

    middleware add to src

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const { registration, login, profileUpdate } = require("../controller/userController")
-const authentication = require("../../middleware/authentication")
+const authentication = require("../middleware/authentication")
 const router = express.Router()
 
 
